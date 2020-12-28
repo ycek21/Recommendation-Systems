adaaf04933de3b1eb473cff1b76e9abd4571aefb
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-12-28 19:05:47 +0100

    Add missing day save

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from data_splitter import partnerDataSplitter
 from partner_data_reader import PartnerDataReader
 from optimizer import Optimizer
 import json
+from datetime import datetime
 
 # partnerDataSplitter.split_dataSet_into_partners()
 
 # partner_04A66CE7327C6E21493DA6F3B9AACC75 = PartnerDataReader(
 #     '04A66CE7327C6E21493DA6F3B9AACC75')
 
-# # partner_C0F515F0A2D0A5D9F854008BA76EB537 = PartnerDataReader(
-# #     'C0F515F0A2D0A5D9F854008BA76EB537')
+partner_C0F515F0A2D0A5D9F854008BA76EB537 = PartnerDataReader(
+    'C0F515F0A2D0A5D9F854008BA76EB537')
 
 
 # optimizer = Optimizer(partner_04A66CE7327C6E21493DA6F3B9AACC75.grouped_by_day)
@@ -20,13 +21,19 @@ import json
 # optimizer.log_optimized_days('04A66CE7327C6E21493DA6F3B9AACC75')
 
 
-with open('resources/validation_data/partner_riegiel_id_04A66CE7327C6E21493DA6F3B9AACC75.json') as notMyFile:
-    Rigieldata = json.load(notMyFile)
+optimizer = Optimizer(partner_C0F515F0A2D0A5D9F854008BA76EB537.grouped_by_day)
+
+for index in range(len(optimizer.data_from_partner) - 1):
+    optimizer.optimize_day(optimizer.data_from_partner[index])
+
+optimizer.log_optimized_days('C0F515F0A2D0A5D9F854008BA76EB537')
+
+# with open('resources/validation_data/partner_riegiel_id_04A66CE7327C6E21493DA6F3B9AACC75.json') as notMyFile:
+#     Rigieldata = json.load(notMyFile)
 
 
-with open('logs_partner_id_04A66CE7327C6E21493DA6F3B9AACC75.json') as MyFile:
-    myData = json.load(MyFile)
-    print(type(myData))
-    # print(myData)
+# with open('logs_partner_id_04A66CE7327C6E21493DA6F3B9AACC75.json') as MyFile:
+#     myData = json.load(MyFile)
+#     print(type(myData))
 
-print(myData == Rigieldata)
+# print(myData == Rigieldata)
--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,7 @@
 import random
 import pandas as pd
 import json
+from datetime import datetime, timedelta
 
 
 class Optimizer():
@@ -8,6 +9,7 @@ class Optimizer():
         self.data_from_partner = data_from_partner
         self.products_seen_so_far = []
         self.optimized_days = []
+        self.previous_day = None
 
     def optimize_day(self, today_df: pd.DataFrame):
         products_to_exclude_tomorrow = []
@@ -22,6 +24,8 @@ class Optimizer():
         products_actually_excluded_tomorrow = todays_products_as_set.intersection(
             products_to_exclude_tomorrow_as_set)
 
+        self._add_missing_days(today_df['click_timestamp'].iloc[0])
+
         optimized_day = {
             "day": today_df['click_timestamp'].iloc[0],
             "productsSeenSoFar": self.products_seen_so_far,
@@ -32,10 +36,36 @@ class Optimizer():
         self.optimized_days.append(optimized_day)
 
         product_seen_so_far_set = set(self.products_seen_so_far)
-
         product_seen_so_far_set.update(todays_products_as_set)
 
         self.products_seen_so_far = sorted(list(product_seen_so_far_set))
+        self.previous_day = today_df['click_timestamp'].iloc[0]
+
+    def _add_missing_days(self, today_date):
+        if(self.previous_day == None):
+            return
+
+        today_date_as_date = datetime.strptime(today_date, '%Y-%m-%d')
+        previous_date_as_date = datetime.strptime(
+            self.previous_day, '%Y-%m-%d')
+
+        dates_subtraction = today_date_as_date - previous_date_as_date
+
+        if dates_subtraction.days > 1:
+            for i in range(dates_subtraction.days - 1):
+                day_to_add = today_date_as_date - \
+                    ((dates_subtraction.days - 1) + i)
+
+                self.optimized_days.append(
+                    self._generate_empty_day(day_to_add))
+
+    def _generate_empty_day(self, date):
+        return {
+            "day": date,
+            "productsSeenSoFar": [],
+            "productsToExclude": [],
+            "productsActuallyExcluded": []
+        }
 
     def log_optimized_days(self, partner_id):
 
