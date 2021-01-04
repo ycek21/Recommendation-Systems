83a9fb4b04dc4c9faf503989e2818f4b4f416230
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2021-01-04 22:16:17 +0100

    Delete jupyter file

--- src/main.py
+++ src/main.py
@@ -8,10 +8,15 @@ from simulation_core import SimulationCore
 simulationCore = SimulationCore()
 
 partner_id = 'C0F515F0A2D0A5D9F854008BA76EB537'
+second_partner_id = '04A66CE7327C6E21493DA6F3B9AACC75'
 
 simulationCore.run_simulation_for_particular_partner(
     partner_id)
 
+simulationCore.run_simulation_for_particular_partner(
+    second_partner_id)
 
 print(simulationCore.check_logs_with_validation_data(
     partner_id))
+print(simulationCore.check_logs_with_validation_data(
+    second_partner_id))
